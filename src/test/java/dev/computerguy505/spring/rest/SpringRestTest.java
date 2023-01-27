568a1bea8d57faa08ab33425ed9550f8f41aa8eb
ade <50022614+computerGUY505@users.noreply.github.com>
2023-01-27 20:05:38 +0400

    Initial Commit

--- src/test/java/dev/computerguy505/spring/rest/SpringRestTest.java
+++ src/test/java/dev/computerguy505/spring/rest/SpringRestTest.java
@@ -7,7 +7,7 @@ package dev.computerguy505.spring.rest;
  *
  * @author ade
  */
-public class SpringRest {
+public class SpringRestTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
